e5f18b11c911d8cdd929ca54f57dbe5757c57ae2
Amiri Barksdale at Work <abarksdale@campusexplorer.com>
2013-07-19 12:28:48 -0700

    Basic functionality working--cleanup now

--- lib/Net/DNS/SPF/Expander.pm
+++ lib/Net/DNS/SPF/Expander.pm
@@ -4,6 +4,8 @@ use Moose;
 use Net::DNS::ZoneFile;
 use Net::DNS::Resolver;
 use MooseX::Types::IO::All 'IO_All';
+use List::MoreUtils qw(any);
+use Scalar::Util ();
 use Data::Printer;
 
 has 'input_file' => (
@@ -33,6 +35,11 @@ has 'spf_records' => (
     isa        => 'Maybe[ArrayRef[Net::DNS::RR]]',
     lazy_build => 1,
 );
+has 'resolver' => (
+    is         => 'ro',
+    isa        => 'Net::DNS::Resolver',
+    lazy_build => 1,
+);
 
 has 'to_expand' => (
     is      => 'ro',
@@ -46,7 +53,7 @@ has 'to_copy' => (
     is      => 'ro',
     isa     => 'ArrayRef[RegexpRef]',
     default => sub {
-        [ qr/^ip4/, qr/^ip6/, qr/^ptr/, qr/^exists/, ];
+        [ qr/v=spf1/, qr/^ip4/, qr/^ip6/, qr/^ptr/, qr/^exists/, ];
     },
 );
 
@@ -54,15 +61,23 @@ has 'to_ignore' => (
     is      => 'ro',
     isa     => 'ArrayRef[RegexpRef]',
     default => sub {
-        [ qr/^(\??)all/, qr/^exp/, ];
+        [ qr/^v=spf1/, qr/^(\??)all/, qr/^exp/, qr/^~all/ ];
     },
 );
 
 has 'expansions' => (
-    is => 'rw',
-    isa => 'HashRef',
-    default => sub {{}},
+    is      => 'rw',
+    isa     => 'HashRef',
+    default => sub { {} },
 );
+
+sub _build_resolver {
+    my $self = shift;
+    return Net::DNS::Resolver->new(
+        recurse => 1,
+        #debug => 1,
+    );
+}
 
 sub _build_destination_file {
     my $self = shift;
@@ -71,13 +86,16 @@ sub _build_destination_file {
 
 sub _build_parsed_file {
     my $self = shift;
-    return Net::DNS::ZoneFile->new( $self->input_file);
+    my $path = $self->input_file->filepath;
+    my $name = $self->input_file->filename;
+    return Net::DNS::ZoneFile->new("${path}${name}");
 }
 
 sub _build_resource_records {
-    my $self             = shift;
-    warn p $self->parsed_file;
-    my @resource_records = $self->parsed_file->readfh;
+    my $self = shift;
+
+    #warn p $self->parsed_file;
+    my @resource_records = $self->parsed_file->read;
     return \@resource_records;
 }
 
@@ -86,7 +104,8 @@ sub _build_spf_records {
 
     # This is crude but correct: SPF records can be both TXT and SPF.
     my @spf_records =
-      grep { $_->txtdata =~ /v=spf1/ } @{ $self->resource_records };
+      grep { $_->txtdata =~ /v=spf1/ }
+      grep { $_->can('txtdata') } @{ $self->resource_records };
     return \@spf_records;
 }
 
@@ -94,31 +113,90 @@ sub write {
     my $self = shift;
 }
 
-sub _expand_spf_component {
+sub _normalize_component {
     my ( $self, $component ) = @_;
-    if (scalar(split(' ', $component))) {
-        my @components = split(' ', $component);
+    my $return = $component;
+    $return =~ s/^.+?://g;
+
+    #warn "Return for $component is $return";
+    return $return;
+}
+
+sub _perform_expansion {
+    my ( $self, $component ) = @_;
+
+    #warn "My component in expansion is: ", p $component;
+    $component = $self->_normalize_component($component);
+    my $packet = $self->resolver->search( $component, 'TXT', 'IN' );
+    my ($answer) = $packet->answer;
+
+    #warn p $answer;
+    #warn "Is my answer blessed? ", Scalar::Util::blessed($answer);
+    my $data = $answer->txtdata;
+
+    #warn "Data: $data";
+    return $data;
+}
+
+sub _expand_spf_component {
+    my ( $self, $component, $expansions ) = @_;
+    warn "Expansions upon entering sub: ", p $expansions;
+    $expansions ||= [];
+    return unless $component;
+    if ( scalar( split( ' ', $component ) ) > 1 ) {
+
+        #warn "The number of components I split are "
+        #. scalar( split( ' ', $component ) );
+        #warn "In first if for $component";
+        my @components = split( ' ', $component );
         for my $component (@components) {
-            return $component if grep {/$component/} @{$self->to_ignore};
-            return $component if grep {/$component/} @{$self->to_copy};
-            $self->_expand_spf_component($component);
+            $self->_expand_spf_component( $component, $expansions );
         }
     }
-    return $component if grep {/$component/} @{$self->to_ignore};
-    return $component if grep {/$component/} @{$self->to_copy};
+    else {
 
-    $self->_expand_spf_component($component);
+        #warn "In else for $component";
+
+        if ( ( any { $component =~ $_ } @{ $self->to_ignore } ) ) {
+            return;
+        }
+        if ( ( any { $component =~ $_ } @{ $self->to_copy } ) ) {
+
+            #warn "Pushing $component onto expansions";
+            push @{$expansions}, $component;
+
+            #warn "Expansions after pushing: ", p $expansions;
+
+        }
+        else {
+            my $new_component = $self->_perform_expansion($component);
+            $self->_expand_spf_component( $new_component, $expansions );
+        }
+
+#push @{$expansions}, $component if any { $component =~ $_ } @{ $self->to_ignore };
+#push @{$expansions}, $component if any { $component =~ $_ } @{ $self->to_copy };
+#$return->{$component} = $component
+#if any { $component =~ $_ } @{ $self->to_ignore };
+#$return->{$component} = $component
+#if any { $component =~ $_ } @{ $self->to_copy };
+    }
+    warn "Expansions for $component before returning: ", p $expansions;
+    return ( $component, $expansions );
 }
 
 sub expand {
-    my $self = shift;
+    my $self     = shift;
     my %spf_hash = ();
-    for my $spf_record (@{$self->spf_records}) {
-        my @spf_components = split(' ', $spf_record->txtdata);
+    for my $spf_record ( @{ $self->spf_records } ) {
+        my @spf_components = split( ' ', $spf_record->txtdata );
         for my $spf_component (@spf_components) {
-            $spf_hash{$spf_record->name}{$spf_component} = $self->_expand_spf_component($spf_component);            
+            my ( $comp, $expansions ) =
+              $self->_expand_spf_component($spf_component);
+            $spf_hash{ $spf_record->name }{$spf_component} = $expansions;
+
         }
     }
+    warn p %spf_hash;
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -1,32 +1,17 @@
-#
-#===============================================================================
-#
-#         FILE: basic.t
-#
-#  DESCRIPTION: 
-#
-#        FILES: ---
-#         BUGS: ---
-#        NOTES: ---
-#       AUTHOR: YOUR NAME (), 
-# ORGANIZATION: 
-#      VERSION: 1.0
-#      CREATED: 07/15/2013 03:33:28 PM
-#     REVISION: ---
-#===============================================================================
-
 use strict;
 use warnings;
 use lib 'lib';
+
 use Net::DNS::SPF::Expander;
 use IO::All;
 
-use Test::More tests => 1;                      # last test to print
+use Test::More tests => 1;
 
 
 my $file = 't/etc/test_zonefile';
 
 my $expander = Net::DNS::SPF::Expander->new(input_file => $file);
 
-my $parsed = $expander->expand->write;
+my $parsed = $expander->expand;
+
 ok(1==1);
