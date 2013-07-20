5d1797da613b865568540f0ac4a8572be25023a5
Amiri Barksdale at Work <abarksdale@campusexplorer.com>
2013-07-19 19:19:24 -0700

    About ready to make the new records as Net::DNS::RR::SPF records

--- lib/Net/DNS/SPF/Expander.pm
+++ lib/Net/DNS/SPF/Expander.pm
@@ -4,7 +4,7 @@ use Moose;
 use Net::DNS::ZoneFile;
 use Net::DNS::Resolver;
 use MooseX::Types::IO::All 'IO_All';
-use List::MoreUtils qw(any);
+use List::AllUtils qw(sum any part first);
 use Scalar::Util ();
 use Data::Printer;
 
@@ -66,9 +66,23 @@ has 'to_ignore' => (
 );
 
 has 'expansions' => (
-    is         => 'rw',
+    is         => 'ro',
     isa        => 'HashRef',
     lazy_build => 1,
+);
+
+has 'lengths_of_expansions' => (
+    is         => 'ro',
+    isa        => 'HashRef',
+    lazy_build => 1,
+);
+
+has 'maximum_record_length' => (
+    is      => 'ro',
+    isa     => 'Int',
+    default => sub {
+        256 - length('v=spf1 ') + length(' ~all');
+    },
 );
 
 sub _build_resolver {
@@ -109,8 +123,15 @@ sub _build_spf_records {
     return \@spf_records;
 }
 
-sub write {
-    my $self = shift;
+sub _build_lengths_of_expansions {
+    my $self              = shift;
+    my $expansions        = $self->expansions;
+    my $length_per_domain = {};
+    for my $domain ( keys %$expansions ) {
+        my $record_string = join( ' ', @{ $expansions->{$domain}{elements} } );
+        $length_per_domain->{$domain} = length($record_string);
+    }
+    return $length_per_domain;
 }
 
 sub _normalize_component {
@@ -145,7 +166,6 @@ sub _expand_spf_component {
         }
     }
     else {
-
         if ( ( any { $component =~ $_ } @{ $self->to_ignore } ) ) {
             return $component;
         }
@@ -173,8 +193,97 @@ sub _expand {
               $self->_expand_spf_component($spf_component);
             $spf_hash{ $spf_record->name }{$spf_component} = $expansions;
         }
+        my $expansion_elements =
+          $self->_extract_expansion_elements( $spf_hash{ $spf_record->name } );
+        $spf_hash{ $spf_record->name }{elements} = $expansion_elements;
     }
+    warn p %spf_hash;
     return \%spf_hash;
+}
+
+sub _extract_expansion_elements {
+    my ( $self, $expansions ) = @_;
+    my @elements = ();
+    my @leading  = ();
+    my @trailing = ();
+  KEY: for my $key ( keys %$expansions ) {
+        if ( any { $key =~ $_ } @{ $self->to_ignore } ) {
+            next KEY;
+        }
+        if ( ref( $expansions->{$key} ) eq 'ARRAY' ) {
+            for my $expansion ( @{ $expansions->{$key} } ) {
+                push @elements, $expansion;
+            }
+        }
+    }
+    my @return = ( @leading, @elements, @trailing );
+    return \@return;
+}
+
+sub new_spf_records {
+    my $self       = shift;
+    my $lengths    = $self->lengths_of_expansions;
+    my $expansions = $self->expansions;
+
+    my @new_spf_records = ();
+
+    for my $domain ( keys %$lengths ) {
+        my $new_records = [];
+
+        # We need to make sure the SPF record is less than 256 chars,
+        # including the spf version and trailing ~all.
+        if ( $lengths->{$domain} > $self->maximum_record_length ) {
+            $new_records =
+              $self->new_records_from_partition(
+                $expansions->{$domain}{elements} );
+        }
+        else {
+            $new_records =
+              $self->new_records_from_arrayref(
+                $expansions->{$domain}{elements} );
+        }
+        push @new_spf_records, $new_records;
+    }
+    return \@new_spf_records;
+}
+
+sub new_records_from_arrayref {
+    my ( $self, $expansions ) = @_;
+    warn "Expansions in records from arrayref: ", p $expansions;
+     
+}
+
+sub new_records_from_partition {
+    my ( $self, $elements ) = @_;
+    my $record_string = join( ' ', @$elements );
+    my $record_length = length($record_string);
+    my $max_length = $self->max_record_length;
+    my $offset        = 0;
+    my $result        = index( $record_string, ' ', $offset );
+    my @space_indices = ();
+
+    while ( $result != -1 ) {
+        push @space_indices, $result if $result;
+        $offset = $result + 1;
+        $result = index( $record_string, ' ', $offset );
+    }
+    my $number_of_partitions =
+      int( $record_length / $max_length ) +
+      ( ($record_length % $max_length) ? 1 : 0 );
+    my @partitions       = ();
+    my $partition_offset = 0;
+    for my $part ( 1 .. $number_of_partitions ) {
+        my $split_point = first { $_ < $max_length * $part } reverse @space_indices;
+        my $substring = substr( $record_string, $partition_offset, $split_point );
+        push @partitions, [ split( ' ', $substring ) ];
+        $partition_offset = $split_point;
+    }
+    my @return = ();
+    for my $partition (@partitions) {
+        my $result = $self->new_records_from_arrayref($partition);
+        push @return, $result;
+    }
+    return \@return;
 }
 
 1;
